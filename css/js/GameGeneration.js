76c759516f79cb509e12dd86a038b37d54f9732d
Shikyo <dennan_lol@hotmail.com>
2021-08-03 16:04:45 +0200

    Removed duplicated code

--- css/js/GameGeneration.js
+++ css/js/GameGeneration.js
@@ -2,23 +2,6 @@
 
 document.addEventListener("DOMContentLoaded", creatingMap, false);
 
-
-
-/*
-for(let i = 0; i < tileMap01.height; i++)
-{
-    for(let j = 0; j < tileMap01.width; j++)
-    {
-        var asd = checkForObjectsInMapArray(i, j);
-        console.log(i + "," + j + ": " + asd);
-    }
-}
-*/
-
-
-
-
-
 /*Because we loaded SokobanBase.js in the html file before this file, we can access its content directly.*/
 function creatingMap()
 { 
@@ -32,53 +15,45 @@ function creatingMap()
     {
         for(let j = 0; j < tileMap01.width; j++)
         {
-            let whichBlock = checkForObjectsInMapArray(i, j);
-
             let div = document.createElement("div");
             div = generateIds(div, idPart2, idPart4);
 
-            console.log(i + "," + j + ": " + whichBlock);
-            
-            if(whichBlock !== null)
-            {
-                div = generateClass(div, whichBlock);
-            }
-            else
-            {
-                div = generateClass(div, "Space");
-            }
+            let className = getRightClassName(div, i, j);
+
+            if(className !== null)
+                div.setAttribute("class", className); 
 
             divMap.appendChild(div);
         }
     }
 }
 
-function checkForObjectsInMapArray(x, y)
+function getRightClassName(div, x, y)
 {
-    let whichBlockFound = null;
-
-    //console.log(tileMap01.mapGrid[x][y].toString());
+    let className = null;
 
     if(tileMap01.mapGrid[x][y].toString() === "W")
     {
-        whichBlockFound = "W";
+        className = Tiles.Wall;
     }
     else if(tileMap01.mapGrid[x][y].toString()  === "B")
     {
-        whichBlockFound = "B";
+        className = Entities.Block;
     }
     else if(tileMap01.mapGrid[x][y].toString()  === "G")
     {
-        whichBlockFound = "G";
+        className = Tiles.Goal;
     }
     else if(tileMap01.mapGrid[x][y].toString()  === "P")
     {
-        whichBlockFound = "P";
+        className = Entities.Character;
+    }
+    else
+    {
+        className = Tiles.Space;
     }
 
-    
-
-    return whichBlockFound;
+    return div;
 }
 
 function generateIds(tile, idPart2, idPart4)
@@ -93,41 +68,9 @@ function generateIds(tile, idPart2, idPart4)
         idPart2++;
     }
 
-
     let fullId  = idPart1 + idPart2 + idPart3 + idPart4;
 
     tile.setAttribute("id", fullId);
 
     return tile;
-}
-
-function generateClass(div, whichBlock)
-{
-    let className = null;
-
-    switch(whichBlock)
-    {
-        case "W":
-            className = Tiles.Wall;
-            break;
-        case "B":
-            className = Entities.Block;
-            break;
-        case "P":
-            className = Entities.Character;
-            break;
-        case "G":
-            className = Tiles.Goal;
-            break;
-        case "Space":
-            className = Tiles.Space;
-            break;
-        default:
-            break;
-    }
-
-    if(className !== null)
-        div.setAttribute("class", className); 
-
-    return div;
 }
